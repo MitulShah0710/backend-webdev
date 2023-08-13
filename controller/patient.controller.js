448b2fd22b019ee4a08b5d56edd1d119fc77b121
MitulShah0710 <mitulshah141516@gmail.com>
2023-08-13 03:33:07 -0300

    upload

--- controller/patient.controller.js
+++ controller/patient.controller.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 const Patient = require("../models/patient");
 const cors = require("cors");
-
+const multer = require("multer");
 router.use(cors());
 router.use(express.json());
+
+const upload = multer();
 
 router.post("/createPatient", async function (req, res) {
   const body = req.body;
