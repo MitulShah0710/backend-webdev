9d08a090d4c15e76d51a25c4779af2c6b988ac9d
MitulShah0710 <mitulshah141516@gmail.com>
2023-08-14 10:38:29 -0300

    patient

--- controller/patient.controller.js
+++ controller/patient.controller.js
@@ -11,7 +11,7 @@ router.use(express.json());
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/'); // Upload directory
+    cb(null, path.join(__dirname, 'uploads')); // Absolute path
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
@@ -22,38 +22,38 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-// router.post("/createPatient", async function (req, res) {
-//   const body = req.body;
-//   console.log("body", body);
-//   const userID = body.userId;
-//   const { firstName, lastName, email, phoneNo, gender, city, address, choiceOfHospitality } = body
-//   // const findPatient = await Patient.findOne({ userID });
-//   // console.log("findPatient", findPatient);
-//   // if (findPatient !== null) {
-//   // return res.status(500).send({
-//   //       message: "Patient already exists",
-//   //       body: findPatient
-//   //   });
-//   // }
-//   console.log("No patient found");
-//   const addPatient = await Patient.create({
-//     userId: userID,
-//     firstName,
-//     lastName,
-//     email,
-//     phoneNo,
-//     address,
-//     city,
-//     gender,
-//     choiceOfHospitality,
-//   });
-//   const patient = await Patient.findOne({userID});
-//   console.log("patient", patient);
-//   res.send({
-//         message: "User added successfully",
-//         body: addPatient
-//     });
-// });
+router.post("/createPatient", async function (req, res) {
+  const body = req.body;
+  console.log("body", body);
+  const userID = body.userId;
+  const { firstName, lastName, email, phoneNo, gender, city, address, choiceOfHospitality } = body
+  // const findPatient = await Patient.findOne({ userID });
+  // console.log("findPatient", findPatient);
+  // if (findPatient !== null) {
+  // return res.status(500).send({
+  //       message: "Patient already exists",
+  //       body: findPatient
+  //   });
+  // }
+  console.log("No patient found");
+  const addPatient = await Patient.create({
+    userId: userID,
+    firstName,
+    lastName,
+    email,
+    phoneNo,
+    address,
+    city,
+    gender,
+    choiceOfHospitality,
+  });
+  const patient = await Patient.findOne({userID});
+  console.log("patient", patient);
+  res.send({
+        message: "User added successfully",
+        body: addPatient
+    });
+});
 
 router.post('/uploadProfilePicture', upload.single('profilePicture'), async (req, res) => {
   try {
@@ -120,39 +120,39 @@ router.delete("/deletePatient/:id", async function (req, res) {
   return res.status(200).send({ message: "Patient Deleted successfully" , body: patients});
 });
 
-router.post('/uploadProfilePicture', upload.single('profilePicture'), async (req, res) => {
-  try {
-    const userId = req.body.userId;
+// router.post('/uploadProfilePicture', upload.single('profilePicture'), async (req, res) => {
+//   try {
+//     const userId = req.body.userId;
 
-    // Check if userId is a valid ObjectId
-    if (!mongoose.Types.ObjectId.isValid(userId)) {
-      return res.status(400).json({ message: 'Invalid userId format' });
-    }
+//     // Check if userId is a valid ObjectId
+//     if (!mongoose.Types.ObjectId.isValid(userId)) {
+//       return res.status(400).json({ message: 'Invalid userId format' });
+//     }
 
-    // Find the user by userId
-    const user = await Patient.findById(userId);
-    console.log("user", user);
-    console.log("file", req.file);
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
-    }
+//     // Find the user by userId
+//     const user = await Patient.findById(userId);
+//     console.log("user", user);
+//     console.log("file", req.file);
+//     if (!user) {
+//       return res.status(404).json({ message: 'User not found' });
+//     }
 
-    if (!req.file) {
-      return res.status(400).json({ message: 'No file uploaded' });
-    }
+//     if (!req.file) {
+//       return res.status(400).json({ message: 'No file uploaded' });
+//     }
 
-    // Update the profile picture field in the user document
-    user.profilePicture = req.file.buffer;
+//     // Update the profile picture field in the user document
+//     user.profilePicture = req.file.buffer;
 
-    await user.save();
-    const user1 = await Patient.findById(userId);
-    console.log("user1", user1);
-    res.status(200).json({ message: 'Profile picture uploaded successfully' });
-  } catch (error) {
-    console.error('Error uploading profile picture:', error);
-    res.status(500).json({ message: 'An error occurred while uploading the profile picture' });
-  }
-});
+//     await user.save();
+//     const user1 = await Patient.findById(userId);
+//     console.log("user1", user1);
+//     res.status(200).json({ message: 'Profile picture uploaded successfully' });
+//   } catch (error) {
+//     console.error('Error uploading profile picture:', error);
+//     res.status(500).json({ message: 'An error occurred while uploading the profile picture' });
+//   }
+// });
 
 
 module.exports = router;
